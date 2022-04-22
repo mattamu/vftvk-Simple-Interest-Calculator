c8c7aac51f185a42edefe0cbdcbe944be61e3f3d
mattamu <mattamu.itu@gmail.com>
2022-04-22 12:02:43 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
-{
-    p = document.getElementById("principal").value;
-    
+{  
+var principal = document.getElementById("principal").value;
+    var n = Number(principal)
+    if (n<=0) {
+    	alert("Enter a positive number");
+		principal.focus();
+		return false;
+    }
+var rate = document.getElementById("rate").value; 
+var years = document.getElementById("years").value;
+var interest = principal * years * rate /100;
+var year = new Date().getFullYear()+parseInt(years);
+var result = document.getElementById("result");
+result.innerHTML= 'If you deposit ' + (principal) + '<br><br> at an interest rate of ' + (rate) +  '<p>You will receive an amount of <span class="htl">' + (interest) + ',</span></p>'+ 'in the year ' + (year);
+// htl for highlight
 }
-        
+// function for rate
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}      
